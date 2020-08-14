7ead17517f728275de358407fc75e3d73e2c7ea5
don-k-jacob <donkjacob.business@gmail.com>
2020-08-14 15:35:21 +0530

    Revert "updates"
    This reverts commit 35ef87a07beaa45d5886c3ae7385ea04645447dc.

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/don_k_jacob/devaloper/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/don_k_jacob/Documents/GitHub/lipsa/lipsa/ai_ads_web"
-export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_FRAMEWORK_DIR=/Users/don_k_jacob/devaloper/flutter/bin/cache/artifacts/engine/darwin-x64"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
